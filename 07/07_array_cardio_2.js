1e257f21963506426439a36fdc2b0db6e82d3607
m-lamarre <studium.square.pantz@gmail.com>
2018-03-29 12:32:31 +0200

    07 array cardio day 2

--- 07/07_array_cardio_2.js
+++ 07/07_array_cardio_2.js
@@ -25,16 +25,18 @@ const comments = [
 // 	}
 // });
 
-const isAdult = people.some(person => ((new Date()).
-	getFullYear()) - person.year >= 19);
-console.log({isAdult});
+const isAdult = people.some(
+	person => new Date().getFullYear() - person.year >= 19
+);
+console.log({ isAdult });
 
 // Array.prototype.every() // is everyone 19 or older?
 
-const allAdults = people.every(person => (
-	(new Date()).getFullYear()) - person.year >= 19);
+const allAdults = people.every(
+	person => new Date().getFullYear() - person.year >= 19
+);
 
-console.log({allAdults});
+console.log({ allAdults });
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
@@ -48,20 +50,17 @@ console.log({allAdults});
 
 const comment = comments.find(comment => comment.id === 823423);
 
-console.log({comment});
+console.log({ comment });
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
 
 const index = comments.findIndex(comment => comment.id === 823423);
-console.log({index});
+console.log({ index });
 
 // comments.splice(index, 1);
 // console.table({comments});
 
-const newComments = [
-	...comments.slice(0, index),
-	...comments.slice(index + 1)
-];
-console.table({newComments});
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
+console.table({ newComments });
